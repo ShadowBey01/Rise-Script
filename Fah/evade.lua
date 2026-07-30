e7225ceeb0157aa1f5f7e182865140360c94e940
HzShadow <darknw211@gmail.com>
2026-07-30 23:11:23 +0300

    Create evade.lua

---
+++ Fah/evade.lua
@@ -0,0 +1,3 @@
+-- You can't crack the Luraph script, bro
+
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/27a9a1994fa47d00d216ea2f0fcb0f49dd0253623f4417ba3f275d7011134529/download"))()
