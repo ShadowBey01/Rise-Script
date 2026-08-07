65d066a179d6d489e7d259bf60fe0a8b39e01122
HzShadow <darknw211@gmail.com>
2026-08-07 14:23:17 +0300

    Update evade.lua

--- Fah/evade.lua
+++ Fah/evade.lua
@@ -300,4 +300,4 @@ protect = "pcall(function() delfile('risearmor-cache.lua') end) local hzshawde p
 
 
 
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/27a9a1994fa47d00d216ea2f0fcb0f49dd0253623f4417ba3f275d7011134529/download"))()
+loadstring(game:HttpGet("https://rise-script-loader.vercel.app/scripts/loader.lua"))()
